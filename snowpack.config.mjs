77222c62ea59d90b05c181961bf1031ac217ee67
Jake Baker <j.baker@airlst.com>
2021-07-17 15:35:19 +0200

    building for gh-pages

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -7,5 +7,11 @@ export default {
   devOptions: {
     tailwindConfig: './tailwind.config.js',
   },
+  buildOptions: {
+    // put the build files in /docs
+    out: 'docs',
+    // put the meta snowpack build files under snowpack instead of _snowpack since Github special-cases underscore prefixed folders
+    metaUrlPath: 'snowpack',
+  },
   plugins: ['@snowpack/plugin-postcss'],
 };
